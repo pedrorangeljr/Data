c2be166389579c5f170953ae9c913377cf5e4e10
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2023-05-19 10:30:20 -0300

    calculo com datas

---
+++ src/Calculo.java
@@ -0,0 +1,27 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Calculo {
+
+	public static void main(String[] args)throws Exception {
+
+	  Calendar calendar = Calendar.getInstance();
+	  
+	  /*Simular que data do banco de dados*/
+	  
+	  calendar.setTime(new SimpleDateFormat("dd-MM-yyyy").parse("10-03-2023")); // Define um data qualquer
+	  
+	  calendar.add(Calendar.DAY_OF_MONTH, -40);
+	  
+	  System.out.println("Somando dia do mês: " + new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));
+	  
+	  calendar.add(Calendar.MONTH, 1);
+	  
+	  System.out.println("Somando o mês: " + new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));
+	  
+	  calendar.add(Calendar.YEAR, 1);
+	  
+	  System.out.println("Somando 1 ano: " + new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));
+	}
+
+}
